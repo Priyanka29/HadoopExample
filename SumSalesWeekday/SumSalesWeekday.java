2121a46b9ab2c5afdabd49cd79b8915797dca704
Priyanka29 <priyankavyas_scs@yahoo.com>
2014-06-24 12:19:23 -0700

    Initial Version

---
+++ SumSalesWeekday/SumSalesWeekday.java
@@ -0,0 +1,39 @@
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class SumSalesWeekday extends Configured implements Tool{
+		
+	
+	public int run(String[] args) throws Exception{
+		Job job = new Job();
+	    job.setJarByClass(SumSalesWeekday.class);
+	    job.setJobName("SumSalesWeekday");
+
+	    FileInputFormat.addInputPath(job, new Path(args[0]));
+	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+	    
+	    job.setMapperClass(SumSalesWeekdayMapper.class);
+	    job.setCombinerClass(SumSalesWeekdayReducer.class);
+	    job.setReducerClass(SumSalesWeekdayReducer.class);
+
+	    job.setOutputKeyClass(Text.class);
+	    job.setOutputValueClass(FloatWritable.class);
+	    
+	    return job.waitForCompletion(true) ? 0 : 1;
+	}
+	
+  public static void main(String[] args) throws Exception {
+	  int exitCode = ToolRunner.run(new SumSalesWeekday(), args);
+	  System.exit(exitCode);
+    
+   }
+}
---
+++ SumSalesWeekday/SumSalesWeekdayMapper.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+
+
+public class SumSalesWeekdayMapper 
+  extends Mapper<LongWritable, Text, Text, FloatWritable> {
+
+  @Override
+  public void map(LongWritable key, Text value, Context context)
+      throws IOException, InterruptedException {
+	  String[] list = value.toString().split("\t");
+	 SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+	 DateFormat format1= new SimpleDateFormat("EEE");
+	 try {
+		Date d = format.parse(list[0]);
+		String day = format1.format(d);
+			context.write(new Text(day), new FloatWritable(Float.parseFloat(list[4])));
+
+	} catch (ParseException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+		
+  }
+
+}
+
+    
