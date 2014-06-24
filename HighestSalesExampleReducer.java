278074d595a99afedd557f4954a024a95ca3a515
Priyanka29 <priyankavyas_scs@yahoo.com>
2014-06-23 21:23:20 -0700

    Intial Project Version

---
+++ HighestSalesExampleReducer.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class HighestSalesExampleReducer
+  extends Reducer<Text, FloatWritable, Text, FloatWritable> {
+  
+  @Override
+  public void reduce(Text key, Iterable<FloatWritable> values,
+      Context context)
+      throws IOException, InterruptedException {
+    
+      float max = 0;
+      
+      for (FloatWritable value : values) {
+    	  if(max < value.get()){
+    		  max = value.get();
+    	  }
+        
+      }
+    context.write(key, new FloatWritable(max));
+  }
+
+}
+
