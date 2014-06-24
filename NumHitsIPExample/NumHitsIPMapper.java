7129b68de9e6a236ca51d702ba3c594223ace6a0
Priyanka29 <priyankavyas_scs@yahoo.com>
2014-06-24 12:33:06 -0700

    Intial Version

---
+++ NumHitsIPExample/NumHitsIPMapper.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class NumHitsIPMapper 
+  extends Mapper<LongWritable, Text, Text, IntWritable> {
+	private static final IntWritable one = new IntWritable(1);
+  @Override
+  public void map(LongWritable key, Text value, Context context)
+      throws IOException, InterruptedException {
+	  Pattern pattern = Pattern.compile("(10.99.99.186)",Pattern.CASE_INSENSITIVE);
+	  Matcher matcher = pattern.matcher(value.toString());
+		while (matcher.find()) {
+			context.write(new Text(matcher.group(0)),new IntWritable(1) );
+		 }
+  }
+
+}
+
+    
